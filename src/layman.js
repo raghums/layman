1f9d1385ba0d984aaf3ac70cbacfd8278e05e784
Raghuram Sreenath <raghums@gmail.com>
2012-07-03 20:17:48 +0530

    first commit

---
+++ src/layman.js
@@ -0,0 +1,28 @@
+/*
+ * layman
+ * https://github.com/raghums/layman
+ *
+ * Copyright (c) 2012 Raghuram Sreenath
+ * Licensed under the MIT, GPL licenses.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
